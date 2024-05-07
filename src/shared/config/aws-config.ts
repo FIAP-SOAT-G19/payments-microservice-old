b989461872acaff9af3aeceb61080d053fb0017e
Rodrigo dos Santos <rodrigo.contato@live.com>
2024-05-07 19:05:07 -0300

    chore: remove config

--- src/shared/config/aws-config.ts
+++ src/shared/config/aws-config.ts
@@ -1,7 +1,7 @@
 export const awsConfig = {
-  region: 'us-east-1',
+  region: '',
   credentials: {
-    accessKeyId: 'AKIA6GBMCEIXOE2MJWJU',
-    secretAccessKey: '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'
+    accessKeyId: '',
+    secretAccessKey: ''
   }
 }
